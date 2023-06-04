b3d41112cbe60fc1a6e94411b46ee7b6f5bb7019
dasd <dasd412@naver.com>
2023-06-04 17:30:57 +0900

    디렉토리 변경 및 react elements

--- root/practice/start_of_npm/src/index.js
+++ root/practice/start_of_npm/src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Library from './chapter_03/library';
+import Clock from './chapter_04/Clock';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Library />
-  </React.StrictMode>
-);
+
+// 매 초 Clock 컴포넌트가 새로이 생성됨.
+setInterval(()=>{
+  root.render(
+    <React.StrictMode>
+      <Clock />
+    </React.StrictMode>
+  );
+},1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
